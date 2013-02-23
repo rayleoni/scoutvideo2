94631b8acc7282c494e96a114df13b45eb63fe83
Ray Lee <ray@heyscout.com>
2013-02-23 01:46:08 -0500

    letmesleeppls

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -1,3 +1,7 @@
 class Scout < ActiveRecord::Base
   attr_accessible :date, :email, :name, :offer
+  validates_presence_of :date,
+                        :email,
+                        :name,
+                        :offer
 end
